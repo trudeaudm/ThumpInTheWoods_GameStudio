6b46deeb4435904de2518af0a3eb795f1212d9d6
barrettand <barrett.and@husky.neu.edu>
2017-02-06 21:39:15 -0500

    parallax
    and tiling

--- Assets/ParallaxScript.cs
+++ Assets/ParallaxScript.cs
@@ -6,14 +6,28 @@ public class ParallaxScript : MonoBehaviour {
 
     bool hasleft, hasright;
     public float moverate;
+    float spriteWidth;
 	// Use this for initialization
 	void Start () {
-
+        spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        hasleft = transform.position.x + GetComponent<SpriteRenderer>().bounds.size.x / 2 < Camera.main.transform.position.x + Screen.width/2;
+        if (!hasleft || !hasright) {
+            float camHorizontalExtend = Camera.main.orthographicSize * Screen.width / Screen.height;
+            float edgeVisiblePositionRight = (transform.position.x + spriteWidth / 2) - camHorizontalExtend;
+            float edgeVisiblePositionLeft = (transform.position.x - spriteWidth / 2) + camHorizontalExtend;
+            if (Camera.main.transform.position.x >= edgeVisiblePositionRight && !hasright) {
+                MakeANewBuddy(1);
+                hasright = true;
+            }
+            if (Camera.main.transform.position.x <= edgeVisiblePositionLeft && !hasleft)
+            {
+                MakeANewBuddy(-1);
+                hasleft = true;
+            }
+        }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(-moverate, 0, 0);
@@ -21,6 +35,18 @@ public class ParallaxScript : MonoBehaviour {
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(moverate, 0, 0);
+        }
+    }
+
+    void MakeANewBuddy(int rightorleft) {
+        Vector3 newposition = new Vector3(transform.position.x + spriteWidth * rightorleft, transform.position.y, transform.position.z);
+        Transform newBuddy = Instantiate(transform, newposition, transform.rotation) as Transform;
+        if (rightorleft > 0) {
+            newBuddy.GetComponent<ParallaxScript>().hasleft = true;
+        }
+        else if (rightorleft < 0)
+        {
+            newBuddy.GetComponent<ParallaxScript>().hasright = true;
         }
     }
 }
