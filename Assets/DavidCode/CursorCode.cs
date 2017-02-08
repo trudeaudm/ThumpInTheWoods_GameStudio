95b23eda7f45f4b86eebc4d2d11f24f4116db0c2
David Trudeau <trudeaudm@gmail.com>
2017-02-08 15:43:39 -0500

    -UI art adds -Built the hotbar and cursor prefab.

---
+++ Assets/DavidCode/CursorCode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorCode : MonoBehaviour {
+    [SerializeField]private Transform particles;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Cursor.visible = false;
+        transform.position = Input.mousePosition;
+        Vector3 particlePos;
+        particlePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        particlePos.z = -2.0f;
+        particles.position = particlePos;
+	}
+}
