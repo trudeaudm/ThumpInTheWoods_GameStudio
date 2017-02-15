6008243c512e5d53a44fc89916ba84d44561eb4e
barrettand <barrett.and@husky.neu.edu>
2017-02-15 16:21:24 -0500

    Lerps
    Lerp a Derp

--- Assets/ObstacleScript.cs
+++ Assets/ObstacleScript.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class ObstacleScript : MonoBehaviour {
 
     public float interp = 0;
-	// Use this for initialization
-	void Start () {
+    bool windtrigger = false;
+    Vector3 prevpos;
+    // Use this for initialization
+    void Start () {
         Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), FindObjectOfType<GhostMove>().GetComponentsInChildren<EdgeCollider2D>()[0], true);
         Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), FindObjectOfType<GhostMove>().GetComponentsInChildren<EdgeCollider2D>()[1], true);
         Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), FindObjectOfType<GhostMove>().GetComponentsInChildren<EdgeCollider2D>()[2], true);
         Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), FindObjectOfType<GhostMove>().GetComponentsInChildren<EdgeCollider2D>()[3], true);
+        prevpos = transform.position;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        if (windtrigger && interp < 1) {
+            interp += 0.01f;
+        }
+        if (interp == 1)
+        {
+            windtrigger = false;
+        }
+        if (gameObject.name == "Wall")
+        {
+            transform.position = Vector3.Lerp(prevpos, prevpos + new Vector3(100, 50, 0), interp);
+        }
         if (gameObject.name == "Spinning Wall")
         {
             transform.eulerAngles = new Vector3(0, 0, Mathf.LerpAngle(0, -90, interp));
-            transform.position = Vector3.Lerp(new Vector3(36.56f, 0.27f, 0), new Vector3(40.3f, -4.5f, 0), interp);
         }
     }
 
@@ -38,11 +51,11 @@ public class ObstacleScript : MonoBehaviour {
     void OnParticleCollision(GameObject other) {
         if (gameObject.name == "Wall")
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(1000, 200));
+            windtrigger = true;
         }
         if (gameObject.name == "Spinning Wall")
         {
-            interp += 0.01f;
+            windtrigger = true;
         }
     }
 }
